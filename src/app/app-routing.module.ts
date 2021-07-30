00aead472f9a0eb8e5c5c807c7251a1130cec32f
akidwell <akidwell@gaig.com>
2021-07-30 09:43:01 -0400

    okta changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,39 @@ import { RouterModule, Routes } from '@angular/router';
 import { ApplicationsComponent } from './applications/applications.component';
 import { PageNotFoundComponent } from './page-not-found-component';
 import { ReportsComponent } from './reports/reports.component';
+import {OKTA_CONFIG, OktaAuthModule, OktaCallbackComponent, OktaAuthGuard} from '@okta/okta-angular';
+const CALLBACK_PATH = 'callback/';
+
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule) },
+  { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule), },
   { path: 'import', loadChildren: () => import('./import/import.module').then(m => m.ImportModule) },
   { path: 'policy', loadChildren: () => import('./policy/policy.module').then(m => m.PolicyModule) },
   { path: 'reports', component: ReportsComponent },
   { path: 'applications', component: ApplicationsComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent },
+   { path: CALLBACK_PATH, component: OktaCallbackComponent }
 ];
 
+const config = {
+  clientId: '0oa11jhvuyHgG4nJl1d7',
+  issuer: 'https://logindev.gaig.com',
+  Secret: 'p_U6WvtlClPwzm0KkMJunJN70qVklUUo5D-ebsrD',
+  redirectUri: 'http://localhost:4200',
+  scopes: ['openid', 'profile', 'email'],
+  pkce: true
+};
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes),    
+     OktaAuthModule,
+  ],
+  exports: [RouterModule],
+
+  providers: [
+    { provide: OKTA_CONFIG, useValue: config },
+  ],
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'rsps-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RSPS';
+  isAuthenticated: boolean = false;
+
+  constructor(public oktaAuth: OktaAuthService) {
+    // Subscribe to authentication state changes
+    this.oktaAuth.$authenticationState.subscribe(
+      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+    );
+  }
+
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+    
+  }
+
+  login() {
+    this.oktaAuth.signInWithRedirect({
+      originalUri: '/'
+    });    
+
+    
+  }
 }
