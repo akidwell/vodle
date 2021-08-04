d63deb54354cf8f69c46172acbc5db16b7108689
akidwell <akidwell@gaig.com>
2021-08-04 09:49:51 -0400

    okta updates- moved from routing module to app module, using nav component instead of app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ApplicationsComponent } from './applications/applications.component';
 import { PageNotFoundComponent } from './page-not-found-component';
 import { ReportsComponent } from './reports/reports.component';
-import {OKTA_CONFIG, OktaAuthModule, OktaCallbackComponent, OktaAuthGuard} from '@okta/okta-angular';
+import { OktaCallbackComponent} from '@okta/okta-angular';
 const CALLBACK_PATH = 'callback';
 
 
@@ -12,27 +12,17 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'import', loadChildren: () => import('./import/import.module').then(m => m.ImportModule) },
+  { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule), },
+  { path: 'policy', loadChildren: () => import('./policy/policy.module').then(m => m.PolicyModule) },
   { path: 'reports', component: ReportsComponent },
   { path: 'applications', component: ApplicationsComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
-const config = {
-  clientId: '0oa13ty5ui2LT2Osn1d7',
-  issuer: 'https://logindev.gaig.com',
-  redirectUri: 'callback',
-  scopes: ['openid', 'profile', 'email'],
-  pkce: true
-};
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),    
-     OktaAuthModule,
   ],
   exports: [RouterModule],
-
-  providers: [
-    { provide: OKTA_CONFIG, useValue: config },
-  ],
 })
 export class AppRoutingModule { }
