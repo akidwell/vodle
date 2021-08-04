206ee2223769fc4b7fd87b41fb3f1c710e6b43de
akidwell <akidwell@gaig.com>
2021-08-04 09:16:18 -0400

    okta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,26 +4,23 @@ import { ApplicationsComponent } from './applications/applications.component';
 import { PageNotFoundComponent } from './page-not-found-component';
 import { ReportsComponent } from './reports/reports.component';
 import {OKTA_CONFIG, OktaAuthModule, OktaCallbackComponent, OktaAuthGuard} from '@okta/okta-angular';
-const CALLBACK_PATH = 'callback/';
+const CALLBACK_PATH = 'callback';
 
 
 const routes: Routes = [
+  { path: CALLBACK_PATH, component: OktaCallbackComponent },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule), },
   { path: 'import', loadChildren: () => import('./import/import.module').then(m => m.ImportModule) },
-  { path: 'policy', loadChildren: () => import('./policy/policy.module').then(m => m.PolicyModule) },
   { path: 'reports', component: ReportsComponent },
   { path: 'applications', component: ApplicationsComponent },
   { path: '**', component: PageNotFoundComponent },
-   { path: CALLBACK_PATH, component: OktaCallbackComponent }
 ];
 
 const config = {
-  clientId: '0oa11jhvuyHgG4nJl1d7',
+  clientId: '0oa13ty5ui2LT2Osn1d7',
   issuer: 'https://logindev.gaig.com',
-  Secret: 'p_U6WvtlClPwzm0KkMJunJN70qVklUUo5D-ebsrD',
-  redirectUri: 'http://localhost:4200',
+  redirectUri: 'callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,32 @@ import { OktaAuthService } from '@okta/okta-angular';
 export class AppComponent {
   title = 'RSPS';
   isAuthenticated: boolean = false;
+  userName: string | undefined;
+
 
   constructor(public oktaAuth: OktaAuthService) {
     // Subscribe to authentication state changes
     this.oktaAuth.$authenticationState.subscribe(
-      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated,
+
     );
   }
 
   async ngOnInit() {
+    const accessToken = this.oktaAuth.getAccessToken();
     this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+
+    const userClaims = await this.oktaAuth.getUser();
+
+    // user name is exposed directly as property
+    this.userName = userClaims.name;
+
     
   }
 
   login() {
     this.oktaAuth.signInWithRedirect({
-      originalUri: '/'
+      originalUri: '/home'
     });    
 
     
