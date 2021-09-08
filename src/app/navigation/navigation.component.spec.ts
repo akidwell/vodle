9b63face318aa7af93374e4d68b9c67f2b09a17b
Glen Moore <gmoore@gaig.com>
2021-09-08 07:40:00 -0400

    Small test changes

--- src/app/navigation/navigation.component.spec.ts
+++ src/app/navigation/navigation.component.spec.ts
@@ -2,6 +2,8 @@ import { HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { OktaAuthModule, OktaAuthService, OKTA_CONFIG } from '@okta/okta-angular';
+import { of } from 'rxjs';
+import { UserAuth } from '../authorization/user-auth';
 
 import { NavigationComponent } from './navigation.component';
 
@@ -10,21 +12,27 @@ describe('NavigationComponent', () => {
   let fixture: ComponentFixture<NavigationComponent>;
   let mockOktaService: OktaAuthService;
   let httpTestingController: HttpTestingController;
+  let mockUserAuth;
+  let mockUserAuth2;
 
   const oktaConfig = {
     issuer: 'https://not-real.okta.com',
     clientId: 'fake-client-id',
     redirectUri: 'http://localhost:4200'
-  };
+  }
   
   beforeEach(async () => {
+    mockUserAuth = jasmine.createSpyObj(['isAuthenticated'])
+    mockUserAuth.isAuthenticated.and.returnValue(true);
+
     await TestBed.configureTestingModule({
       imports: [OktaAuthModule],
       declarations: [ NavigationComponent],
-      providers: [OktaAuthService,  { provide: OKTA_CONFIG, useValue: oktaConfig }]
+      providers: [OktaAuthService,  { provide: OKTA_CONFIG, useValue: oktaConfig }, { provide: UserAuth, useValue: mockUserAuth }]
     })
     .compileComponents();
 
+    
     httpTestingController = TestBed.inject(HttpTestingController);
     mockOktaService = TestBed.inject(OktaAuthService);
   });
