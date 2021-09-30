0a6a61cfc1a66e78962c91fba9b1452a09ef0b28
Cody Sturgill <csturgill@GAIG.COM>
2021-09-30 15:48:56 -0400

    coverage sequence value

--- src/app/policy/coverages/coverages.component.ts
+++ src/app/policy/coverages/coverages.component.ts
@@ -11,11 +11,18 @@ import { EndorsementCoveragesGroup } from './coverages';
 export class CoveragesComponent implements OnInit {
   endorsementCoveragesGroups! : EndorsementCoveragesGroup[];
   formStatus: any;
+  coveragesSequence: number = -1;
+
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
       this.endorsementCoveragesGroups = data['endorsementCoveragesGroups'].endorsementCoveragesGroups;
+      //This flattens the sequence number over all the coverages data and gets the highest value. This value will be used for adding any new coverage.
+      this.coveragesSequence = this.endorsementCoveragesGroups.map(g => g.coverages.map(c => c.sequence)).reduce(
+        (r, seq) => r.concat(seq.map(s => s)),[]).reduce(
+          (a,b) => Math.max(a,b));
     });
   }
   @Output() status: EventEmitter<any> = new EventEmitter();
