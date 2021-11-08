4644b5c1187cc07bc74fb6cf5cc98af307447ec4
Glen Moore <gmoore@gaig.com>
2021-11-08 06:43:21 -0500

    Catch Auth errors for ignore

--- src/app/authorization/auth.service.ts
+++ src/app/authorization/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { UserAuth } from './user-auth';
 import { ConfigService } from '../config/config.service';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { IAuthObject } from './auth-object';
 import { OktaAuthService } from '@okta/okta-angular';
@@ -15,14 +15,14 @@ import { Router } from '@angular/router';
 
 export class AuthService {
   tokenJSON: IAuthObject | undefined;
-  constructor(private userAuth: UserAuth, private http: HttpClient, private config: ConfigService, private jwtHelper: JwtHelperService, private oktaAuth: OktaAuthService,private router: Router) {
+  constructor(private userAuth: UserAuth, private http: HttpClient, private config: ConfigService, private jwtHelper: JwtHelperService, private oktaAuth: OktaAuthService, private router: Router) {
     // Subscribe to authentication state changes
     this.oktaAuth.$authenticationState.subscribe(isAuthenticated => {
-      console.log('first ',isAuthenticated)
+      console.log('first ', isAuthenticated)
       if (isAuthenticated) {
         this.login2();
       } else {
-        console.log('okta login',isAuthenticated)
+        console.log('okta login', isAuthenticated)
         this.login();
       }
     });
@@ -33,43 +33,57 @@ export class AuthService {
     return !!(await this.oktaAuth.tokenManager.get('accessToken'));
   }
 
-header = {
-  headers: new HttpHeaders()
-    .set('OKTA-Authorization',  'Bearer ' + this.oktaAuth.getAccessToken())
+  header = {
+    headers: new HttpHeaders()
+      .set('OKTA-Authorization', 'Bearer ' + this.oktaAuth.getAccessToken())
   }
 
   // GAM - TEMP - Header test
   private _headers = new HttpHeaders();
 
-  login(){
-  this.oktaAuth.signInWithRedirect({
-    originalUri: '/home'
-  });
-}
-login2() {
-  this.userAuth.init();
-  this.getAuthToken().subscribe({
-    next: token => {
-      this.userAuth.bearerToken = JSON.parse(JSON.stringify(token)).data;
-      // GAM - TEMP - New observable 
-      this.userAuth.ApiBearerToken = JSON.parse(JSON.stringify(token)).data;
-      console.log(token)
-      localStorage.setItem('jwt_token', this.userAuth.bearerToken);
-    },
-    //error: err => this.errorMessage = err
-  });
-}
-  getAuthToken(): Observable<string>{   
+  login() {
+    this.oktaAuth.signInWithRedirect({
+      originalUri: '/home'
+    });
+  }
+
+  login2() {
+    this.userAuth.init();
+    this.getAuthToken().subscribe({
+      next: token => {
+        if (token != null && token != "") {
+          this.userAuth.bearerToken = JSON.parse(JSON.stringify(token)).data;
+          // GAM - TEMP - New observable 
+          this.userAuth.ApiBearerToken = JSON.parse(JSON.stringify(token)).data;
+          console.log(token)
+          localStorage.setItem('jwt_token', this.userAuth.bearerToken);
+        }
+      },
+      error: err => console.log("!!! " + err) 
+    });
+  }
+
+  getAuthToken(): Observable<string> {
     // GAM - TEMP - Header test
     const headers = this._headers.append('OKTA-Authorization', 'Bearer ' + this.oktaAuth.getAccessToken());
-    //return  this.http.get<string>(this.config.apiBaseUrl + 'api/userauth/gettoken', this.header)
-    return  this.http.get<string>(this.config.apiBaseUrl + 'api/userauth/gettoken', { headers : headers })
-    .pipe(
-      tap(token => {
-        this.processAuthClaims(this.jwtHelper.decodeToken(JSON.parse(JSON.stringify(token)).data));
-      }),
+    return this.http.get<string>(this.config.apiBaseUrl + 'api/userauth/gettoken', { headers: headers })
+      .pipe(
+        tap(async token => {
+          if (token == null) {
+            console.log("No token");
+            //await  this.logout();
+          }
+          else {
+            this.processAuthClaims(this.jwtHelper.decodeToken(JSON.parse(JSON.stringify(token)).data));
+          }
+        }),
+        catchError(err => {
+          console.log("Error: " + err.message);
+          return "";
+        })
       )
   }
+
   processAuthClaims(token: IAuthObject) {
     console.log(token);
     this.userAuth.userName = token.name;
@@ -83,12 +97,10 @@ login2() {
     this.userAuth.userRole = token.userRole;
 
     console.log(this.userAuth);
-    if (this.userAuth.isAuthenticated == 'False' )
-    {
+    if (this.userAuth.isAuthenticated == 'False') {
       this.router.navigate(['/access-denied']);
     }
-    else if (this.router.url == '/access-denied')
-    {
+    else if (this.router.url == '/access-denied') {
       this.router.navigate(['/home']);
     }
   }
--- src/app/error-handling/global-error-handler.ts
+++ src/app/error-handling/global-error-handler.ts
@@ -4,47 +4,52 @@ import { Subscription } from 'rxjs';
 import { ErrorService } from './error.service';
 import { JobLoggerParameter } from './job-logger-parameter';
 import { JobLoggerService } from './job-logger.service';
-import { JobLoggerResponse} from './job-logger-response';
+import { JobLoggerResponse } from './job-logger-response';
 
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   response!: JobLoggerResponse;
-    
-  constructor(private  injector: Injector) { }
+
+  constructor(private injector: Injector) { }
 
   sub!: Subscription;
 
-    handleError(error: Error | HttpErrorResponse) {
-        const errorService = this.injector.get(ErrorService)
-        const jobLoggerService = this.injector.get(JobLoggerService)
+  handleError(error: Error | HttpErrorResponse) {
+    const errorService = this.injector.get(ErrorService)
+    const jobLoggerService = this.injector.get(JobLoggerService)
 
-        let errorMessage;
-        let stackTrace;
-        let parm: JobLoggerParameter = {source: "", message: "", data: "" };
+    let errorMessage;
+    let stackTrace;
+    let parm: JobLoggerParameter = { source: "", message: "", data: "" };
 
-        if (error instanceof HttpErrorResponse) {
-            // Server Error
-            errorMessage = errorService.getServerMessage(error);
-            stackTrace = errorService.getServerStack(error);
-            parm = { source: stackTrace, message: errorMessage, data: "" };
-            this.sub = jobLoggerService.postJobLogger(parm).subscribe({
-              next: jobLoggerResponse => {
-                this.response = jobLoggerResponse;}
-              })
-            console.log(errorMessage);
-            window.alert(errorMessage);
-          } else {
-            // Client Error
-            errorMessage = errorService.getClientMessage(error);
-            stackTrace = errorService.getClientStack(error);
-            parm = {source: "Angular", message: errorMessage, data: "" };
-            this.sub = jobLoggerService.postJobLogger(parm).subscribe({
-              next: jobLoggerResponse => {
-                this.response = jobLoggerResponse;}
-              })
-            console.log(errorMessage);
-            window.alert(errorMessage)      
-            }
+    if (error instanceof HttpErrorResponse) {
+      // Server Error
+      errorMessage = errorService.getServerMessage(error);
+      stackTrace = errorService.getServerStack(error);
+      parm = { source: stackTrace, message: errorMessage, data: "" };
+      this.sub = jobLoggerService.postJobLogger(parm).subscribe({
+        next: jobLoggerResponse => {
+          this.response = jobLoggerResponse;
+        }
+      })
+      console.log(errorMessage);
+      window.alert(errorMessage);
     }
+    else if (error.message.includes("AuthApiError")) {
+      // Do nothing for now
+    } else {
+      // Client Error
+      errorMessage = errorService.getClientMessage(error);
+      stackTrace = errorService.getClientStack(error);
+      parm = { source: "Angular", message: errorMessage, data: "" };
+      this.sub = jobLoggerService.postJobLogger(parm).subscribe({
+        next: jobLoggerResponse => {
+          this.response = jobLoggerResponse;
+        }
+      })
+      console.log(errorMessage);
+      window.alert(errorMessage)
+    }
+  }
 }
