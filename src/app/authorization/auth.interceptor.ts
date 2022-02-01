4ea70829dc423faeffac96c6c6c813ab2dc3d9f7
Cody Sturgill <csturgill@GAIG.COM>
2022-02-01 08:34:28 -0500

    token expiration check

--- src/app/authorization/auth.interceptor.ts
+++ src/app/authorization/auth.interceptor.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
 import { HttpInterceptor,HttpRequest, HttpEvent, HttpHandler } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(){};
+    constructor(private jwtHelper: JwtHelperService){};
 
     intercept(req: HttpRequest<any>,
               next: HttpHandler): Observable<HttpEvent<any>> {
         const idToken = localStorage.getItem("jwt_token");
-        if (idToken) {
+        if (idToken && this.isTokenValid(idToken)) {
             const cloned = req.clone({
                 headers: req.headers.set("Authorization",
                     "Bearer " + idToken)
@@ -19,5 +20,17 @@ export class AuthInterceptor implements HttpInterceptor {
         else {
             return next.handle(req);
         }
+    }
+
+    isTokenValid(token : string | null): boolean {
+      if (token == null) {
+        return false;
+      }
+      const expirationDate = this.jwtHelper.getTokenExpirationDate(token);
+      if (expirationDate != null && expirationDate > new Date()) {
+        return true;
+      } else {
+        return false;
+      }
     }
 }
