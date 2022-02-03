19091b6d566478faec0a1c2d95ba074f1d9a3cc0
Cody Sturgill <csturgill@GAIG.COM>
2022-02-03 09:30:17 -0500

    Routing test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,9 @@ export function onAuthRequired(_oktaAuth: OktaAuthService) {
   });
 }
 const routes: Routes = [
-  { path: CALLBACK_PATH, component: OktaCallbackComponent },
+  { path: CALLBACK_PATH, component: OktaCallbackComponent , canActivate: [OktaAuthGuard, AuthGuard] , data: {
+    onAuthRequired
+  }  },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) , canActivate: [OktaAuthGuard, AuthGuard] , data: {
     onAuthRequired
   }  },
