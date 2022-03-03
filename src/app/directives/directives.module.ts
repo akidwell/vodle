59987558a4616c9c76b2e5dd1f5ab4b28c217416
Glen Moore <gmoore@gaig.com>
2022-03-03 13:30:17 -0500

    Bugfix for retro date

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from "@angular/core";
 import { ClickStopPropagation } from "./click-stop-propagation.directive";
-import { DateRangeValidator } from "./date-range";
 import { MaxIntegerValidator } from "./max-integer.directive";
 import { NullValueDirective } from "./null-value.directive";
 import { StringToCurrencyDirective } from "./string-to-currency";
-import { StringToDateDirective } from "./string-to-date";
 
 @NgModule({
     imports: [],
@@ -12,17 +10,13 @@ import { StringToDateDirective } from "./string-to-date";
         NullValueDirective,
         MaxIntegerValidator,
         StringToCurrencyDirective,
-        ClickStopPropagation,
-        StringToDateDirective,
-        DateRangeValidator
+        ClickStopPropagation
     ],
     exports: [
         NullValueDirective,
         MaxIntegerValidator,
         StringToCurrencyDirective,
-        ClickStopPropagation,
-        StringToDateDirective,
-        DateRangeValidator
+        ClickStopPropagation
     ]
 })
 export class DirectivesModule { }
