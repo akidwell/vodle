d3967e88e2399348a8a591b2fc442c0fc869e2a8
akidwell <akidwell@gaig.com>
2022-03-30 12:15:21 -0400

    test2

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -12,7 +12,7 @@ describe('HomeComponent', () => {
     })
     .compileComponents();
   });
-//test
+//test2
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
