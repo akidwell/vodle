8fc98f95b9a2a2bb89fc8e2862614cda1144c2b8
Glen Moore <gmoore@gaig.com>
2022-05-10 09:32:47 -0400

    LInt cleanup of core objects

--- src/app/core/authorization/auth.guard.ts
+++ src/app/core/authorization/auth.guard.ts
@@ -13,6 +13,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.authService.isAuthenticated()};
+    return this.authService.isAuthenticated();
+  }
 
 }
--- src/app/core/services/format-date/format-date-display.service.ts
+++ src/app/core/services/format-date/format-date-display.service.ts
@@ -7,17 +7,15 @@ import * as moment from 'moment';
 })
 export class FormatDateForDisplay {
 
-  constructor() { }
-
   public formatDateForDisplay(date: Date | moment.Moment | null) : string | null {
-    var displayString: string | null;
+    let displayString: string | null;
     if (date == null) {
       return null;
     }
     if (moment.isMoment(date)){
       displayString = date.format('MM/DD/YYYY');
     } else {
-      var datePipe =  new DatePipe('en-US');
+      const datePipe = new DatePipe('en-US');
       displayString = datePipe.transform(date, 'MM/dd/yyyy');
     }
     return displayString;
