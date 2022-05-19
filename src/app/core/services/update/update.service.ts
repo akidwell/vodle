13ef11263d46a45c8bd4e06c3a3828b0621c213e
Glen Moore <gmoore@gaig.com>
2022-05-19 14:59:11 -0400

    PWA - Service Worker changes

---
+++ src/app/core/services/update/update.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
+import { ConfirmationDialogService } from '../confirmation-dialog/confirmation-dialog.service';
+import { MessageDialogService } from '../message-dialog/message-dialog-service';
+
+@Injectable()
+export class UpdateService {
+
+  constructor(private updates: SwUpdate, private confirmationDialogService: ConfirmationDialogService, private messageDialogServive: MessageDialogService) { }
+
+  startTimer() {
+    const updateInterval$ = interval(30 * 1000);
+    updateInterval$.subscribe(() => {
+      this.updates.checkForUpdate().then(() => console.log('checking for updates'));
+    });
+  }
+
+  startLogging() {
+    this.updates.versionUpdates.subscribe(evt => {
+      console.log(evt.type);
+      switch (evt.type) {
+      case 'VERSION_DETECTED':
+        console.log(`Downloading new app version: ${evt.version.hash}`);
+        break;
+      case 'VERSION_READY':
+        console.log(`Current app version: ${evt.currentVersion.hash}`);
+        console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+        break;
+      case 'VERSION_INSTALLATION_FAILED':
+        console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+        break;
+      }
+    });
+  }
+
+  startConfirmation() {
+    this.updates.versionUpdates.subscribe(async event => {
+      if (event.type == 'VERSION_READY' && await this.confirmationDialogService.open('New Version Available','Update?')) {
+        this.updates.activateUpdate().then(() => document.location.reload());
+      }
+    });
+  }
+
+  startUnrecoverableStateCheck() {
+    this.updates.unrecoverable.subscribe(event => {
+      this.messageDialogServive.open('Error','An error occurred that we cannot recover from:\n' +
+            event.reason +
+            '\n\nPlease reload the page.');
+    });
+  }
+}
