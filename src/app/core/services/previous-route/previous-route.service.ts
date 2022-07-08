5e9f24bcc8e6768a50ad0f8ec06f90e8f6cc5755
Glen Moore <gmoore@gaig.com>
2022-07-08 15:16:45 -0400

    Bugfix for prev

--- src/app/core/services/previous-route/previous-route.service.ts
+++ src/app/core/services/previous-route/previous-route.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ConsoleService } from '@ng-select/ng-select/lib/console.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
@@ -20,7 +21,7 @@ export class PreviousRouteService {
   public getPreviousUrlFormatted() {
     if (this.previousUrl.value != '') {
       const path = this.previousUrl.value.split('/');
-      if (path.length > 1) {
+      if (path.length > 1 && path[1][0] !== undefined) {
         return 'Previous - ' + path[1][0].toUpperCase() + path[1].slice(1) + (path[2] !== undefined ? ' #' + path[2] : '');
       }
     }
