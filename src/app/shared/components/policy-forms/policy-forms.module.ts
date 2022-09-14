7a5b4d5b22d2d591f5dc59f8f20195812682365e
Glen Moore <gmoore@gaig.com>
2022-09-14 10:47:40 -0400

    Adding authenticating message

--- src/app/shared/components/policy-forms/policy-forms.module.ts
+++ src/app/shared/components/policy-forms/policy-forms.module.ts
@@ -5,11 +5,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DirectivesModule } from '../../directives/directives.module';
 import { CanDeactivateGuard } from 'src/app/features/policy/guards/can-deactivate-guard';
-import { PolicyFormsComponent } from './policy-forms.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { SpecimenPacketService } from './services/policy-forms.service';
-import { PolicyFormVariableComponent } from './policy-form-variable/policy-form-variable.component';
 import { ScrollToTopModule } from 'src/app/core/components/scroll-to-top/scroll-to-top.module';
+import { PolicyFormVariableComponent } from './policy-form-variable/policy-form-variable.component';
+import { PolicyFormsComponent } from './policy-forms/policy-forms.component';
 
 @NgModule({
   declarations: [
