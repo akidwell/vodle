79e335b5dccf6df2a70aa6596c91d3151870b220
Glen Moore <gmoore@gaig.com>
2022-10-17 13:02:27 -0400

    Bugfix

--- src/app/shared/classes/policy-form-class.ts
+++ src/app/shared/classes/policy-form-class.ts
@@ -33,28 +33,6 @@ export abstract class PolicyFormClass implements PolicyForm, Validation, QuoteAf
   canAdd = true;
   checkboxClass = '';
 
-  get isIncluded(): boolean {
-    return this._isIncluded;
-  }
-  set isIncluded(value: boolean) {
-    this._isDirty = true;
-    this._isIncluded = value;
-    if (!value && this.isSelected)
-    {
-      this.checkboxClass = 'mandatory';
-    }
-    else {
-      this.checkboxClass = '';
-    }
-  }
-
-  get formData(): VariableFormData[] | null {
-    return this._formData;
-  }
-  set formData(value: VariableFormData[] | null) {
-    this._isDirty = true;
-    this._formData = value;
-  }
   constructor(policyForm?: PolicyForm){
     if (policyForm) {
       this.existingInit(policyForm);
@@ -65,6 +43,22 @@ export abstract class PolicyFormClass implements PolicyForm, Validation, QuoteAf
     this.validate();
   }
 
+  get isIncluded(): boolean {
+    return this._isIncluded;
+  }
+  set isIncluded(value: boolean) {
+    this._isDirty = true;
+    this._isIncluded = value;
+    this.checkIncludedClass();
+  }
+
+  get formData(): VariableFormData[] | null {
+    return this._formData;
+  }
+  set formData(value: VariableFormData[] | null) {
+    this._isDirty = true;
+    this._formData = value;
+  }
   get validationResults(): QuoteValidationClass {
     return this._validationResults;
   }
@@ -96,6 +90,16 @@ export abstract class PolicyFormClass implements PolicyForm, Validation, QuoteAf
     this._errorMessages = value;
   }
 
+  private checkIncludedClass() {
+    if (!this._isIncluded && this.isSelected)
+    {
+      this.checkboxClass = 'mandatory';
+    }
+    else {
+      this.checkboxClass = '';
+    }
+  }
+
   existingInit(policyForm: PolicyForm){
     this.formName = policyForm.formName;
     this.formTitle = policyForm.formTitle;
@@ -112,6 +116,7 @@ export abstract class PolicyFormClass implements PolicyForm, Validation, QuoteAf
     this.isSelected = policyForm.isSelected;
     this._isIncluded = policyForm.isIncluded;
     this._formData = policyForm.formData;
+    this.checkIncludedClass();
   }
 
   newInit() {
